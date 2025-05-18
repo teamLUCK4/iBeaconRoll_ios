7e3e70b25cb8dcd6761544b405561db28c003fd8
HaileyKim <rlaekdus17@sookmyung.ac.kr>
2025-05-18 18:29:09 +0900

    info.plist 비콘 세팅 추가

---
+++ ibeaconroll_ios/RangeBeaconView.swift
@@ -0,0 +1,18 @@
+//
+//  RangeBeaconView.swift
+//  ibeaconroll_ios
+//
+//  Created by 김다연 on 5/18/25.
+//
+
+import SwiftUI
+
+struct RangeBeaconView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RangeBeaconView()
+}
